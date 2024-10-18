6d7e44a5ec6d9ff3363481bfe5742c2bd655729a
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-10-18 14:15:04 +0300

    14:14

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+  console.log(contacts);
+  const newContacts = Array(number).fill(0).map(createFakeContact);
+  console.log(newContacts);
+};
 
 generateContacts(5);
